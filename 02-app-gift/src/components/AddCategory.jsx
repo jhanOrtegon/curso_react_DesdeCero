d093015c01ae6c681af4b4df572e5bfdee6df439
jhan carlos ortegon <jhancarlos.ortegon@gmail.com>
2022-04-07 22:57:30 -0500

    cambios

--- 02-app-gift/src/components/AddCategory.jsx
+++ 02-app-gift/src/components/AddCategory.jsx
@@ -7,13 +7,16 @@ const AddCategory = ({ setCategories }) => {
     const handleSubmit = (e) => {
         e.preventDefault();
         setCategories((e) => [...e, stateInput.inputCategory])
+        setStateInput({ inputCategory: '' })
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <input
                 onChange={(e) => setStateInput({ [e.target.name]: e.target.value })}
-                type="text" name='inputCategory' />
+                type="text" name='inputCategory'
+                value={stateInput.inputCategory}
+            />
         </form>
     )
 }
